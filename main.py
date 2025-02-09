9adc1097dbdb6a45af19c441ccb9f842b7fbb0e8
ninann <ninann@class-docker>
2025-02-09 02:31:27 +0000

    setting up class

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+
+class SignalDetection:
+    def __init__(self, hits, misses, false_alarms, correct_rejections):
+        self.hits = hits
+        self.misses = misses
+        self.false_alarms = false_alarms
+        self.correct_rejections = correct_rejections
