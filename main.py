659cb586bf41d9e05bd5a8db08861ba33c91d948
ninann <ninann@class-docker>
2025-02-09 02:55:06 +0000

    code setup

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ class SignalDetection:
         self.misses = misses
         self.false_alarms = false_alarms
         self.correct_rejections = correct_rejections
+        
+    def hit_rate(self):
+        H = self.hits / (self.hits + self.misses)
+        return H
+
+    def false_alarm_rate(self):
+        FA = self.false_alarms / (self.false_alarms + self.correct_rejections) 
+        return FA
+    
+def d_prime(self):
+        
+def criterion(self):
+    
